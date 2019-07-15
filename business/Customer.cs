1fb02ec58c73764e11a148ca87b84d2c09633124
David Flynn <TheGidYinFlynn@users.noreply.github.com>
2019-07-15 16:15:33 +0100

    Add files via upload

---
+++ business/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace business
+{
+    class Customer
+    {
+
+        string fullName;
+        string address;
+        int customerId;
+    }
+}
---
+++ business/Guest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace business
+{
+    class Guest
+    {
+        private string fullName;
+        private string passportNumber;
+        private int bookingId;
+
+
+        public string Name
+        {
+            get
+            {
+                return fullName;
+
+            }
+
+            set
+            {
+                fullName = value;
+            }
+
+
+        }
+
+        public String passport
+        {
+            get
+            {
+                return passportNumber;
+
+            }
+
+            set
+            {
+                passportNumber = value;
+
+            }
+
+          
+        }
+        
+        public int id
+        {
+            get
+            {
+                return bookingId;
+            }
+
+            
+        }
+    }
+}
+    
